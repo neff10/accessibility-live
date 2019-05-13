632994c95976558c9073adb9b3e782e71d37d7d3
Neff10 <st.tunkara@gmail.com>
2019-05-13 14:54:50 +0100

    Test

--- src/Frontend/App.jsx
+++ src/Frontend/App.jsx
@@ -1,38 +1,19 @@
 import React from 'react';
 import './css/App.css';
 import cardImage from './diala.jpg'
-import NavBar from "./Frontend/component/NavBar/NavBar.js";
 
 
 
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <div className="card">
-        <div className='flipCard'>
-          <div className='frontCard'>
-            <img src={cardImage} alt="photo" />
-            <div className='frontText'>Dial-a-ride</div>
-          </div>
-          <div className='backCard'>
-            <div className='text'>
-              <h1><b>Dial-a-ride</b></h1>
-              <p>Description</p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="card2">
-        <div className="frontCard2">
-          <div className="cardImg">
-            <img src={cardImage} alt="photo" className='screen' />
-          </div>
+      <div className="cardItem">
+        <div className="card">
+          <img src={cardImage} alt="img" className='screen' />
           <div className="cardDetails">
-            <div className='text2'>
+            <div className='text'>
               <h2><b>Dial-a-ride</b></h2>
-              <p>Description</p>
+              <p><h3>Description</h3></p>
             </div>
             <div className='readMore'>
               <p>Read more...</p>
@@ -40,22 +21,6 @@ function App() {
           </div>
         </div>
       </div>
-
-      <div className="card3">
-        <img src={cardImage} alt="photo" className='screen3' />
-        <div className='text3'>
-          <div className='description3'>
-            <div className='title'>
-              <h3><b>Dial-a-ride</b></h3>
-              <h4>Description</h4>
-            </div>
-            <div className='secondpara'>
-              <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ac ultricies mauris, non lacinia felis.</h5>
-            </div>
-          </div>
-        </div>
-      </div>
-
     </div>
   );
 }
